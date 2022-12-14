963af51ba0f62485d1b57ae1612b91c0a00f8215
Serey Roth <sereyratanakroth@gmail.com>
2022-12-13 16:43:09 -0800

    removed second div container

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -4,30 +4,26 @@ import Image from 'next/image'
 import { urlFor } from '../lib/client'
 
 const HeroBanner = ({ heroBanner }) => {
-  return (
-    <div className="hero-banner-container">
-        <div>
+    return (
+        <div className="hero-banner-container">
             <p className="beats-solo">
                 {heroBanner.smallText}
             </p>
             <h3>{heroBanner.midText}</h3>
             <h1>{heroBanner.largeText1}</h1>
-            <Image 
-                src={urlFor(heroBanner.image).url()} 
+            <Image
+                src={urlFor(heroBanner.image).url()}
                 width={500}
                 height={500}
-                alt="headphones" 
+                alt="headphones"
                 className="hero-banner-image" />
-            <div>
-                <Link href={`/product/${heroBanner.product}`}>
-                    <button type="button">{heroBanner.buttonText}</button>
-                </Link>
-                <div className="desc">
-                    <h5>Description</h5>
-                    <p>{heroBanner.desc}</p>
-                </div>
+            <Link href={`/product/${heroBanner.product}`}>
+                <button type="button">{heroBanner.buttonText}</button>
+            </Link>
+            <div className="desc">
+                <h5>Description</h5>
+                <p>{heroBanner.desc}</p>
             </div>
-        </div>
     </div>
   )
 }
